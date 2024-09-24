bc9e7aed6f0a0e0a0c06e84fb67006b5100825d7
wangxuan <wangxuan>
2024-09-24 10:16:22 +0800

    ✨ feat:增加路由

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,20 @@
 -->
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
+    <!-- <TableMove msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TableMove from './components/TableMove.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    // TableMove
   }
 }
 </script>
